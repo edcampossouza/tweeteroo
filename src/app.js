2d120fa5c4d72910c6e6479e03064c7149fa6496
Eduardo Souza <edcampossouza@gmail.com>
2023-01-04 18:39:55 -0300

    wire the routes to the db functions

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 import express from "express";
 import cors from "cors";
+import { getTweets, postTweet, signUp } from "./database.js";
 
 const PORT = 5000;
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.status(200).send("hello Tweteroo");
+app.get("/tweets", (req, res) => {
+  const data = getTweets();
+  res.status(200).send(data);
 });
 
-app.listen(PORT, () => console.log(`listenint on port ${PORT}`));
+app.post("/sign-up", (req, res) => {
+  const r = signUp(req.body);
+  res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+  const data = postTweet(req.body);
+  res.send(data);
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
